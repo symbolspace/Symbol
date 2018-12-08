68b9237e8d849579364a6fa5f015cf4d3497ff6b
symbolspace <symbolspace@outlook.com>
2018-12-08 21:30:38 +0800

    更新nuget信息、升级到C# 7.3

--- src/Symbol.Data/Properties/AssemblyInfo.cs
+++ src/Symbol.Data/Properties/AssemblyInfo.cs
@@ -14,12 +14,13 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("symbolspace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("© symbolspace All rights reserved.")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("data in Symbol")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("4.0.0.2")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("4.0.0.2")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Please see https://github.com/symbolspace/Symbol/wiki/Symbol.Data.dll for more in" +
+    "formation.")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("4.0.0.3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("4.0.0.3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Symbol.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Symbol.Data")]
-[assembly: System.Reflection.AssemblyVersionAttribute("4.0.0.2")]
+[assembly: System.Reflection.AssemblyVersionAttribute("4.0.0.3")]
 [assembly: System.Resources.NeutralResourcesLanguageAttribute("zh-CN")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
