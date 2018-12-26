f0154caf987a5e6769f4de63391b4f17fac16376
symbolspace <symbolspace@outlook.com>
2018-12-27 00:02:47 +0800

    Create AliasAttribute.cs
    支持[Alias("xxx")]特性

---
+++ src/Symbol/Symbol/Formatting/AliasAttribute.cs
@@ -0,0 +1,28 @@
+﻿/*  
+ *  author：symbolspace
+ *  e-mail：symbolspace@outlook.com
+ */
+using System;
+
+namespace Symbol.Formatting {
+
+    /// <summary>
+    /// 标记在序列化时输出的名称。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public sealed class AliasAttribute : Attribute {
+        /// <summary>
+        /// 获取或设置输出名称。
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 创建AliasAttribute实例。
+        /// </summary>
+        /// <param name="name">输出名称。</param>
+        public AliasAttribute(string name) {
+            Name = name;
+        }
+    }
+
+}
