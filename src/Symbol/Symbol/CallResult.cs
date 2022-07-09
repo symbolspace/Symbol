d5d6bd27862cbcebc5b814dc2ec015d57bab2ae0
symbolspace <symbolspace@outlook.com>
2022-07-09 12:42:48 +0800

    增加CallResult 承载类

---
+++ src/Symbol/Symbol/CallResult.cs
@@ -0,0 +1,67 @@
+﻿namespace Symbol;
+
+/// <summary>
+/// 调用返回结果。
+/// </summary>
+public class CallResult {
+
+    /// <summary>
+    /// 是否为成功。
+    /// </summary>
+    public virtual bool IsSuccess { get { return Code == 1000; } }
+    /// <summary>
+    /// 状态码。
+    /// </summary>
+    public virtual int Code { get; set; }
+    /// <summary>
+    /// 状态消息。
+    /// </summary>
+    public virtual string Message { get; set; }
+    /// <summary>
+    /// 返回数据。
+    /// </summary>
+    public virtual object ResultData { get; set; }
+
+    /// <summary>
+    /// 创建实例
+    /// </summary>
+    public CallResult() :
+        this(1000, "", null) {
+    }
+
+    /// <summary>
+    /// 创建实例
+    /// </summary>
+    /// <param name="code">状态码</param>
+    /// <param name="message">状态消息</param>
+    public CallResult(int code, string message)
+        : this(code, message, null) {
+    }
+    /// <summary>
+    /// 创建实例
+    /// </summary>
+    /// <param name="code">状态码</param>
+    /// <param name="message">状态消息</param>
+    /// <param name="resultData">返回数据</param>
+    public CallResult(int code, string message, object resultData) {
+        Code = code;
+        Message = message;
+        ResultData = resultData;
+    }
+    /// <summary>
+    /// 创建实例
+    /// </summary>
+    /// <param name="resultData">返回数据</param>
+    public CallResult(object resultData)
+        : base() {
+        ResultData = resultData;
+    }
+    /// <summary>
+    /// 获取HashCode.
+    /// </summary>
+    /// <returns>返回 <see cref="Code"/></returns>
+    public override int GetHashCode() {
+        return Code;
+    }
+
+}
---
+++ src/Symbol/Symbol/CallResult`1.cs
@@ -0,0 +1,51 @@
+﻿namespace Symbol;
+
+/// <summary>
+/// 调用返回结果（泛型）
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class CallResult<T> : CallResult {
+    /// <summary>
+    /// 返回数据（T）。
+    /// </summary>
+    /// <remarks>尝试转换父类的<see cref="CallResult.ResultData"/>类型为<typeparamref name="T"/></remarks>
+    public virtual T Result {
+        get {
+            return (T)ResultData;
+        }
+    }
+
+    /// <summary>
+    /// 创建实例
+    /// </summary>
+    public CallResult() :
+        base() {
+    }
+
+    /// <summary>
+    /// 创建实例
+    /// </summary>
+    /// <param name="code">状态码</param>
+    /// <param name="message">状态消息</param>
+    public CallResult(int code, string message)
+        : base(code, message) {
+    }
+    /// <summary>
+    /// 创建实例
+    /// </summary>
+    /// <param name="code">状态码</param>
+    /// <param name="message">状态消息</param>
+    /// <param name="result">返回数据</param>
+    public CallResult(int code, string message, T result)
+        : base(code, message) {
+        ResultData = result;
+    }
+    /// <summary>
+    /// 创建实例
+    /// </summary>
+    /// <param name="result">返回数据</param>
+    public CallResult(T result)
+        : base() {
+        ResultData = result;
+    }
+}
